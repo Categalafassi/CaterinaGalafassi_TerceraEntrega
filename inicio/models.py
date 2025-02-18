9a1cab38436aad8e567d6602a3cd9aed0c7ebd76
Caterina <categalafassi@gmail.com>
2025-02-18 17:16:14 -0300

    Inicio

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro:
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    ISBN=models.IntegerField(max_length=13)
+    fecha_publicacion=models.DateField()
+    sinopsis=models.TextField(null=True, blank=True)
+    
